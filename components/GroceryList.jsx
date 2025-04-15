c265b335ca56ad479d9440b08dfda36051119475
DianaWilson1 <shedteckned@gmail.com>
2025-04-14 23:47:49 -0400

    .

--- components/GroceryList.jsx
+++ components/GroceryList.jsx
@@ -1,41 +1,26 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const GroceryList = () => {
   const [groceries, setGroceries] = useState([]);
-  const [error, setError] = useState(null); // new
 
   useEffect(() => {
-    axios.get("http://localhost:3001/groceries")
-      .then(res => {
-        if (Array.isArray(res.data)) {
-          setGroceries(res.data);
-        } else {
-          setError("Invalid data format from server.");
-        }
-      })
-      .catch(err => {
-        console.error("Error fetching groceries:", err);
-        setError("Could not fetch groceries. Please try again.");
-      });
-  }, []);
+      .then(res => setGroceries(res.data))
+    .catch(err => console.error("Error fetching groceries:", err));
+}, []);
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  return (
-    <div>
-      <h1>All Groceries</h1>
-      <ul>
-        {groceries.map(item => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+return (
+  <div>
+    <h1>All Groceries</h1>
+    <ul>
+      {groceries.map(item => (
+        <li key={item.id}>
+          {item.name} - {item.quantity}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 };
 
 export default GroceryList;
