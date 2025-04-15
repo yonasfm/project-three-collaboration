5c9d46009ea8e9a12219380a67dd983eb1ab2138
DianaWilson1 <shedteckned@gmail.com>
2025-04-15 16:41:47 -0400

    add style

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import bagImage from "./images/bag.png";
 
 const Home = () => {
   return (
@@ -7,7 +8,18 @@ const Home = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Groceries App</title>
       <h1>Welcome to the Groceries app!</h1>
-      <p>An app for Grouthrees Market.</p>
+      <p>Welcome! Enjoy hassle-free shopping with fresh picks and doorstep delivery.</p>
+      <p>From fruits to pantry essentials, everything you need is just a tap away.</p>
+      <p>No lines, no heavy bags — just easy, smart grocery shopping!</p>
+      <p>Ready to shop? Let’s get started!</p>
+      <img
+        src={bagImage}
+        alt="Groceries bag"
+        style={{
+          marginLeft: '800px',
+          marginTop: '-323px'
+        }}
+      />
     </>
   );
 };
