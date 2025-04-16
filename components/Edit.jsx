0d6b50fd973b977e3880eec998ab38fcfdc7da4a
User Name <user@email.com>
2025-04-15 19:34:05 -0500

    Work in progress on GroceryList and new GroceryForm

--- components/Edit.jsx
+++ components/Edit.jsx
@@ -10,16 +10,9 @@ const Edit = () => {
           style={{ width: "20%", height: "auto", marginBottom: "20px", borderRadius: "12px" }}
         />
         <p>
-          History:{" "}
+          Edit:{" "}
           <span>
-            Founded in 2015, Grouthrees Market began as a small community grocery store
-            with a vision to provide fresh, locally sourced, and sustainable food options.
-            Over the years, it has grown into a beloved neighborhood market with over 100
-            locations nationwide. Grouthrees is known for its curated selection of organic
-            produce, artisanal goods, and household essentials, all while supporting local
-            farmers and producers. With a customer-first philosophy, we continue to create
-            welcoming spaces where people can shop with confidence, enjoy healthy living,
-            and connect with their communities.
+            Here you will be able to edit your favorite treats and groceries!
           </span>
         </p>
       </div>
