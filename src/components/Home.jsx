c4c1ef670b3cff81681708cb5228da47a58d6c94
DianaWilson1 <shedteckned@gmail.com>
2025-04-16 14:21:59 -0400

    updarting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
 
       <div style={{ margin: '84px' }}>
         <h1>Welcome to the Groceries app!</h1>
-        <p>Welcome! Enjoy hassle-free shopping with fresh picks and doorstep delivery.</p>
+        <p>Welcome! Enjoy hassle-free shopping, with fresh picks and doorstep delivery.</p>
         <p>From fruits to pantry essentials, everything you need is just a tap away.</p>
         <p>No lines, no heavy bags — just easy, smart grocery shopping!</p>
-        <p>Ready to shop? Let’s get started!</p>
+        <p>Ready to shop? Let’s get started! </p>
       </div>
 
       <img
